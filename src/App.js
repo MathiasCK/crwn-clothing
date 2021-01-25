bc3e482e139e6191df538d2a187a7ca72754524b
Mathias Kolberg <mathias.ck2000@gmail.com>
2021-01-25 23:58:48 +0100

    Adding & Styling Header

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@ import HomePage from "./pages/homepage/HomePage.component";
 import { Route, Switch } from "react-router-dom";
 import ShopPage from "./pages/shop/Shop.component";
 import "./App.css";
+import Header from "./components/header/Header.component";
 
 function App() {
   return (
     <div>
+      <Header />
       <Switch>
         <Route exact path="/" component={HomePage} />
         <Route path="/shop" component={ShopPage} />
---
+++ src/components/header/Header.component.jsx
@@ -0,0 +1,50 @@
+import React from "react";
+import styled from "styled-components";
+import { Link } from "react-router-dom";
+import { ReactComponent as Logo } from "../../assets/crown.svg";
+const Header = () => {
+  return (
+    <StyledHeader>
+      <Link className="logo-container" to="/">
+        <Logo className="logo"></Logo>
+      </Link>
+      <Options>
+        <Link className="option" to="/shop">
+          Shop
+        </Link>
+        <Link className="option" to="/contact">
+          Contact
+        </Link>
+      </Options>
+    </StyledHeader>
+  );
+};
+
+const StyledHeader = styled.div`
+  height: 70px;
+  width: 100%;
+  display: flex;
+  justify-content: space-between;
+  margin-bottom: 25px;
+
+  & .logo-container {
+    height: 100%;
+    width: 70px;
+    padding: 25px;
+  }
+`;
+
+const Options = styled.div`
+  width: 50%;
+  height: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: flex-end;
+
+  & .option {
+    text-transform: uppercase;
+    padding: 10px 15px;
+  }
+`;
+
+export default Header;
