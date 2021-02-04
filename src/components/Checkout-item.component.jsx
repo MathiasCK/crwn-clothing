0a37e4109ec4aa9b20a07cd26cf18ce9bb629328
Mathias Kolberg <mathias.ck2000@gmail.com>
2021-02-04 12:12:35 +0100

    Add & Remove Item on checkoutpage

--- src/components/Checkout-item.component.jsx
+++ src/components/Checkout-item.component.jsx
@@ -1,16 +1,33 @@
 import React from "react";
+import { connect } from "react-redux";
 import styled from "styled-components";
+import {
+  clearItemFromCart,
+  addItem,
+  removeItem,
+} from "../redux/cart/cart.actions";
 
-const CheckOutItem = ({ cartItem: { name, imageUrl, price, quantity } }) => {
+const CheckOutItem = ({ cartItem, clearItem, addItem, removeItem }) => {
+  const { name, imageUrl, price, quantity } = cartItem;
   return (
     <StyledCheckoutItem>
       <div className="image-container">
         <img src={imageUrl} alt="Checkout Item" />
       </div>
       <span className="name">{name}</span>
-      <span className="quantity">{quantity}</span>
+      <span className="quantity">
+        <div className="arrow" onClick={() => removeItem(cartItem)}>
+          &#10094;
+        </div>
+        <span className="value">{quantity}</span>
+        <div className="arrow" onClick={() => addItem(cartItem)}>
+          &#10095;
+        </div>
+      </span>
       <span className="price">$ {price}</span>
-      <div className="remove-button">&#10005;</div>
+      <div className="remove-button" onClick={() => clearItem(cartItem)}>
+        &#10005;
+      </div>
     </StyledCheckoutItem>
   );
 };
@@ -39,7 +56,13 @@ const StyledCheckoutItem = styled.div`
   }
 
   .quantity {
-    padding-left: 20px;
+    display: flex;
+    .arrow {
+      cursor: pointer;
+    }
+    .value {
+      margin: 0 10px;
+    }
   }
 
   .remove-button {
@@ -48,4 +71,10 @@ const StyledCheckoutItem = styled.div`
   }
 `;
 
-export default CheckOutItem;
+const mapDispatchToProps = (dispatch) => ({
+  clearItem: (item) => dispatch(clearItemFromCart(item)),
+  addItem: (item) => dispatch(addItem(item)),
+  removeItem: (item) => dispatch(removeItem(item)),
+});
+
+export default connect(null, mapDispatchToProps)(CheckOutItem);
