9fcb4c295a6513a077eac47d3c01303e90540389
Mathias Kolberg <mathias.ck2000@gmail.com>
2021-02-20 21:53:53 +0100

    Responsive

--- client/src/pages/collection/Collection.component.jsx
+++ client/src/pages/collection/Collection.component.jsx
@@ -40,7 +40,7 @@ const StyledCollectionPage = styled(motion.div)`
 
   .items {
     display: grid;
-    grid-template-columns: repeat(auto-fit, minmax(300px, 1fr));
+    grid-template-columns: repeat(auto-fit, minmax(400px, 1fr));
     grid-column-gap: 1rem;
     grid-row-gap: 2rem;
 
